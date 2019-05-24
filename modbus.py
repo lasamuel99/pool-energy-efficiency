ce1a7958a60a55b163c20913f835948a54b5329e
Luke Samuel <SESA505681@se.com>
2019-05-24 15:09:17 -0400

    Changed deviced address to correct one

--- modbus.py
+++ modbus.py
@@ -11,13 +11,13 @@ from datetime import datetime
 conf.SIGNED_VALUES = True
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('10.10.53.13', 502))
+sock.connect(('10.10.53.10', 502))
 
 # Returns a message or Application Data Unit (ADU) specific for doing
 # Modbus TCP/IP.
 starttime=time.time()
 while True:
-    message = tcp.read_holding_registers(slave_id=103, starting_address=501, quantity=9)
+    message = tcp.read_holding_registers(slave_id=2, starting_address=3205, quantity=1)
     response = tcp.send_message(message, sock)
     print(datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f): "), response)
     time.sleep(1.0 - ((time.time() - starttime) % 1.0))
